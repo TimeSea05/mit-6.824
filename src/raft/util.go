8207ab5a207f5c8b45669e5f3d35cf209e81516c
avalanche <anwentao1@gmail.com>
2023-03-18 13:55:05 +0800

    WIP: finish leader election(2A) improve log function

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,13 +1,54 @@
 package raft
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
 
 // Debugging
-const Debug = false
+var debug bool
+var debugStart time.Time
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug {
-		log.Printf(format, a...)
+// event topics
+const (
+	dElection    = "ELECT"
+	dStateChange = "STACH"
+	dVote        = "VOTE "
+	dHeartBeart  = "HBEAT"
+	dLock        = "LOCK "
+	dTimer       = "TIMER"
+	dWaitGroup   = "WGRUP"
+	dRPC         = "RPCTH"
+)
+
+func init() {
+	// initialize debug verbosity
+	verbose := os.Getenv("VERBOSE")
+	if verbose != "" {
+		level, err := strconv.Atoi(verbose)
+		if err != nil {
+			log.Fatal("Invalid verbosity!")
+		}
+
+		if level > 0 {
+			debug = true
+		}
+	}
+
+	// set debug start time
+	debugStart = time.Now()
+}
+
+func DebugLog(topic string, peer int, format string, a ...interface{}) (n int, err error) {
+	if debug {
+		time := time.Since(debugStart).Milliseconds()
+		prefix := fmt.Sprintf("%6d %s PEER %d: ", time, topic, peer)
+		newFmt := prefix + format + "\n"
+
+		fmt.Printf(newFmt, a...)
 	}
 	return
 }
